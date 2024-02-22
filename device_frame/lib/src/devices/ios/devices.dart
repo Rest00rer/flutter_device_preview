b9f261f853ac8bd0c001fd7025bdc9d2652b6faf
Rest0rer <byrestorer@gmail.com>
2024-02-22 11:17:14 +0300

    add 14 pro max

--- device_frame/lib/src/devices/ios/devices.dart
+++ device_frame/lib/src/devices/ios/devices.dart
@@ -1,29 +1,19 @@
 import 'package:device_frame/src/info/info.dart';
 
-import 'package:device_frame/src/devices/ios/iphone_12_mini/device.dart'
-    as i_iphone_12_mini;
-import 'package:device_frame/src/devices/ios/iphone_12/device.dart'
-    as i_iphone_12;
-import 'package:device_frame/src/devices/ios/iphone_12_pro_max/device.dart'
-    as i_iphone_12_pro_max;
+import 'package:device_frame/src/devices/ios/iphone_12_mini/device.dart' as i_iphone_12_mini;
+import 'package:device_frame/src/devices/ios/iphone_12/device.dart' as i_iphone_12;
+import 'package:device_frame/src/devices/ios/iphone_12_pro_max/device.dart' as i_iphone_12_pro_max;
 
-import 'package:device_frame/src/devices/ios/iphone_13_mini/device.dart'
-    as i_iphone_13_mini;
-import 'package:device_frame/src/devices/ios/iphone_13/device.dart'
-    as i_iphone_13;
-import 'package:device_frame/src/devices/ios/iphone_13_pro_max/device.dart'
-    as i_iphone_13_pro_max;
-import 'package:device_frame/src/devices/ios/iphone_se/device.dart'
-    as i_iphone_se;
-import 'package:device_frame/src/devices/ios/ipad_air_4/device.dart'
-    as i_ipad_air_4;
+import 'package:device_frame/src/devices/ios/iphone_13_mini/device.dart' as i_iphone_13_mini;
+import 'package:device_frame/src/devices/ios/iphone_13/device.dart' as i_iphone_13;
+import 'package:device_frame/src/devices/ios/iphone_13_pro_max/device.dart' as i_iphone_13_pro_max;
+import 'package:device_frame/src/devices/ios/iphone_se/device.dart' as i_iphone_se;
+import 'package:device_frame/src/devices/ios/ipad_air_4/device.dart' as i_ipad_air_4;
 import 'package:device_frame/src/devices/ios/ipad/device.dart' as i_ipad;
-import 'package:device_frame/src/devices/ios/ipad_pro_11inches/device.dart'
-    as i_ipad_pro_11inches;
-import 'package:device_frame/src/devices/ios/ipad_pro_12Inches_gen2/device.dart'
-    as i_ipad_12inches_gen2;
-import 'package:device_frame/src/devices/ios/ipad_pro_12Inches_gen4/device.dart'
-    as i_ipad_12inches_gen4;
+import 'package:device_frame/src/devices/ios/ipad_pro_11inches/device.dart' as i_ipad_pro_11inches;
+import 'package:device_frame/src/devices/ios/ipad_pro_12Inches_gen2/device.dart' as i_ipad_12inches_gen2;
+import 'package:device_frame/src/devices/ios/ipad_pro_12Inches_gen4/device.dart' as i_ipad_12inches_gen4;
+import 'package:device_frame/src/devices/ios/iphone_14_pro_max/device.dart' as i_iphone_14_pro_max;
 
 /// A set of iOS devices.
 class IosDevices {
@@ -41,6 +31,7 @@ class IosDevices {
   DeviceInfo get iPadPro11Inches => i_ipad_pro_11inches.info;
   DeviceInfo get iPad12InchesGen2 => i_ipad_12inches_gen2.info;
   DeviceInfo get iPad12InchesGen4 => i_ipad_12inches_gen4.info;
+  DeviceInfo get iPhone14ProMax => i_iphone_14_pro_max.info;
 
   /// All devices.
   List<DeviceInfo> get all => [
@@ -56,5 +47,6 @@ class IosDevices {
         iPadAir4,
         iPad,
         iPadPro11Inches,
+        iPhone14ProMax,
       ];
 }
---
+++ device_frame/lib/src/devices/ios/iphone_14_pro_max/screen.g.dart
@@ -0,0 +1,23 @@
+part of 'device.dart';
+
+// Generated manually with https://fluttershapemaker.com/
+final _screenPath = Path()
+  ..moveTo(135,25)
+  ..cubicTo(81.4284,25,38,68.4284,38,122)
+  ..lineTo(38,1624)
+  ..cubicTo(38,1677.57,81.4284,1721,135,1721)
+  ..lineTo(740,1721)
+  ..cubicTo(793.572,1721,837,1677.57,837,1624)
+  ..lineTo(837,122)
+  ..cubicTo(837,68.4284,793.572,25,740,25)
+  ..lineTo(135,25)
+  ..close()
+  ..moveTo(357.176,54)
+  ..cubicTo(341.159,54,328.176,66.9837,328.176,83)
+  ..cubicTo(328.176,99.0163,341.159,112,357.176,112)
+  ..lineTo(515.784,112)
+  ..cubicTo(531.801,112,544.784,99.0163,544.784,83)
+  ..cubicTo(544.784,66.9837,531.801,54,515.784,54)
+  ..lineTo(357.176,54)
+  ..close()
+  ..fillType = PathFillType.evenOdd;
